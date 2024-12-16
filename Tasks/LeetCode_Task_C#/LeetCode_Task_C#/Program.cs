19f8393bda75771053835850807265b33ae039a7
Mikstyz <ehouz85@gmail.com>
2024-12-16 18:43:24 +0300

    3264 Task

---
+++ Tasks/LeetCode_Task_C#/LeetCode_Task_C#/Program.cs
@@ -0,0 +1,22 @@
+﻿using Task_3264;
+
+namespace Program
+{
+    class Prog
+    {
+        static void Main(string[] args)
+        {
+            int[] Myarr = { 2, 1, 3, 5, 6 };
+            Solution anal = new Solution();
+
+
+            int[] arr = anal.GetFinalState(Myarr, 5, 2);
+
+
+            foreach (int i in arr)
+            {
+                Console.Write(i + ", ");
+            }
+        }
+    }
+}
---
+++ Tasks/LeetCode_Task_C#/LeetCode_Task_C#/Task-3264.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using System;
+
+namespace Task_3264
+{
+    public class Solution
+    {
+        public int[] GetFinalState(int[] nums, int k, int multiplier)
+        {
+
+            for (int i = 0; i < k; i++)
+            {
+                int index = Array.IndexOf(nums, nums.Min());
+                nums[index] *= multiplier;
+
+            }
+
+            return nums;
+        }
+    }
+}
